2f2fb6161ef8a7934a716380829d499378d073e2
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 00:36:36 +0200

    [Feat] implement episodeList component

---
+++ src/Screens/CharacterDetails/EpisodeList/EpisodeList.interface.tsx
@@ -0,0 +1,8 @@
+import { ApolloError } from '@apollo/client';
+import { IEspoide } from '../CharacterDetails.interface';
+
+export interface IEpisodeListProps {
+  episodes: IEspoide[];
+  loading: boolean;
+  error: boolean;
+}
