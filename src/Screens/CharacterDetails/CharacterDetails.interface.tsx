f247b53979019d30d7f2ee411a250fa5055bae2f
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 00:37:42 +0200

    [Feat] implement CharacterDetails screen

---
+++ src/Screens/CharacterDetails/CharacterDetails.interface.tsx
@@ -0,0 +1,34 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+import { ImageSourcePropType } from 'react-native';
+import { ICharactersResult } from '../../graphql/GraphQl.interface';
+import { RootStackParamList } from '../../Navigator/MainStackNavigator';
+
+type CharacterDetailsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'characterDetails'
+>;
+
+export interface ICharacterDetailsProps {
+  navigation: CharacterDetailsScreenNavigationProp;
+  route: { params: ICharactersResult };
+}
+
+enum gender {
+  male = 'male',
+  female = 'female',
+}
+
+export interface IEspoide {
+  id: number;
+  name: string;
+  air_date: Date;
+}
+
+export interface IRickyMortyCharcterDetails {
+  id: number;
+  name: string;
+  image: ImageSourcePropType;
+  species: string;
+  gender: gender;
+  episode: IEspoide[];
+}
---
+++ src/Screens/CharacterDetails/CharacterDetails.styles.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../appStyles';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    marginTop: 5,
+    textAlign: 'center',
+  },
+  characterImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 15,
+  },
+  infoText: {
+    fontSize: 15,
+    marginTop: 5,
+    color: colors.grey6,
+  },
+});
+
+export default styles;
