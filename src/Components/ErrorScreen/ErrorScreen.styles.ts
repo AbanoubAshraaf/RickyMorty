6874933056eac6545dd89d460bcac52f9f93f834
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 00:19:19 +0200

    [Feat] implement error screen to explain errors

---
+++ src/Components/ErrorScreen/ErrorScreen.styles.ts
@@ -0,0 +1,19 @@
+
+import { StyleSheet } from 'react-native';
+import { colors } from '../../appStyles';
+const styles = StyleSheet.create({
+
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    padding:10
+  },
+  errorText:{
+    fontSize:20,
+    color:colors.grey6
+  }
+
+});
+
+export default styles;
