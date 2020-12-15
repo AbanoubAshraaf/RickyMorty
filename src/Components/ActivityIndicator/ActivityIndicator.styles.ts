4dcb71c81f9b9f6bd9578705187341c50bb5b992
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 00:17:58 +0200

    [Feat] implement activityIndicator component

---
+++ src/Components/ActivityIndicator/ActivityIndicator.styles.ts
@@ -0,0 +1,25 @@
+
+import { StyleSheet } from 'react-native';
+import { colors } from '../../appStyles';
+const styles = StyleSheet.create({
+
+  loaderContainer: {
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+    elevation: 5,
+    backgroundColor: colors.mainThemeBackgroundColor,
+    borderRadius: 30,
+    alignSelf: 'center',
+  },
+
+});
+
+export default styles;
---
+++ src/Components/ActivityIndicator/ActivityIndicator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { colors } from '../../appStyles';
+import { IActivityIndicator } from './ActivityIndicator.interface';
+import styles from './ActivityIndicator.styles';
+
+const ActivitIndicator = ({loading}:IActivityIndicator) => {
+
+
+if (!loading)
+return null
+
+  return (
+
+    <View style={styles.loaderContainer}>
+    <ActivityIndicator
+      color={colors.mainTextColor}
+      size={15}
+    />
+  </View>
+  );
+};
+
+export default ActivitIndicator;
