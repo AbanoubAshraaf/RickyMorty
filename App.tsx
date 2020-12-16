4c4b47bcbe3a93feb10edfeb22bf678d547cb7ca
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 07:33:12 +0200

    [Feat] add sentry to ricky morty app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import { rickyClient } from './src/graphql';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainStackNavigator } from './src/Navigator/MainStackNavigator';
 import { LogBox } from 'react-native';
-
+import * as Sentry from "@sentry/react-native";
+Sentry.init({
+  dsn: "https://3c4600d2cfad491f80e635c93b1c54ad@o490678.ingest.sentry.io/5558886",
+});
 const mainApp = () => {
   LogBox.ignoreAllLogs(true)
   return (
