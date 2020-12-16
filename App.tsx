a976783e5947f7c2f1ac9cc7523534bef53f38d5
Abanoub Ashraf <abanoubashrf25@gmail.com>
2020-12-16 06:18:55 +0200

    [Test] fix path of helper functions test

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { rickyClient } from './src/graphql';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainStackNavigator } from './src/Navigator/MainStackNavigator';
+import { LogBox } from 'react-native';
 
 const mainApp = () => {
+  LogBox.ignoreAllLogs(true)
   return (
     <ApolloProvider client={rickyClient}>
       <NavigationContainer>
