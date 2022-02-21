f31b4159b012539c168ce4d102a55b4f458a207e
AbanoubAshraaf <abanoubashrf25@gmail.com>
2022-02-21 03:22:59 +0200

    fix merge new data with old data issue

--- src/graphql/links/helperFunctions.ts
+++ src/graphql/links/helperFunctions.ts
@@ -9,12 +9,13 @@ export const mergeNewData = (
     const { results: icomingResults } = incoming;
     const merged = existingRuslts ? existingRuslts.slice(0) : [];
     const newIds = [...existing.ids];
-    for (let i = 0; i < icomingResults.length; ++i) {
-      if (!newIds.includes(icomingResults[i].id)) {
-        newIds.push(icomingResults[i].id);
-        merged[existingRuslts.length + i] = icomingResults[i];
+    if (icomingResults)
+      for (let i = 0; i < icomingResults.length; ++i) {
+        if (!newIds.includes(icomingResults[i].id)) {
+          newIds.push(icomingResults[i].id);
+          merged[existingRuslts.length + i] = icomingResults[i];
+        }
       }
-    }
     return { ...existing, ...incoming, results: merged, ids: newIds };
   }
   return existing;
